d4ac26899a9cb01b7af964fd74c8f5f4ce565562
swvmpdad <will.g.camp@gmail.com>
2022-10-12 22:57:39 -0500

    added client front end and concurrently

--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ const { ApolloServer } = require('apollo-server-express');
 const { authMiddleware } = require('./utils/auth');
 const { typeDefs, resolvers } = require('./schemas');
 const db = require('./config/connection');
+const path = require('path');
 
 const PORT = process.env.PORT || 3001;
 const server = new ApolloServer({
@@ -21,6 +22,15 @@ const startApolloServer = async (typeDefs, resolvers) => {
   await server.start();
 
   server.applyMiddleware({ app });
+
+  // Serve up static assets
+  if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '../clent/build')));
+  }
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../client/build/index.html'));
+  });
 
   db.once('open', () => {
     app.listen(PORT, () => {
